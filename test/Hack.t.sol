9fa2f8f05729645e78acd70813c3acd6674dfd6e
Rappie <jasper@rappie.com>
2024-07-15 13:50:34 +0200

    WETH

--- test/Hack.t.sol
+++ test/Hack.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8;
 import {Test, console} from "forge-std/Test.sol";
 
 import {ICurve} from "src/interfaces/ICurve.sol";
-
+import {WETH9} from "src/interfaces/WETH9.sol";
 
 contract HackTest is Test {
     function setUp() public {}
