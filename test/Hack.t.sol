3b89a2eb6e61a7f430e75ff23b34de2b3bbeb0aa
Rappie <jasper@rappie.com>
2024-07-15 13:45:24 +0200

    TestHack

--- test/Hack.t.sol
+++ test/Hack.t.sol
@@ -1,24 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract HackTest is Test {
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
+	function testHack() public {
+		console.log("testHack");
+	}
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
-    }
 }
