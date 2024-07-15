5fe59157b5e966f9576e351424490e9b7df4924f
Rappie <jasper@rappie.com>
2024-07-15 14:30:36 +0200

    testHack

--- test/Hack.t.sol
+++ test/Hack.t.sol
@@ -7,10 +7,23 @@ import {ICurve} from "src/interfaces/ICurve.sol";
 import {WETH9} from "src/interfaces/WETH9.sol";
 
 contract HackTest is Test {
-    function setUp() public {}
+    WETH9 WETH = WETH9(payable(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2));
+    ICurve pool = ICurve(0x9848482da3Ee3076165ce6497eDA906E66bB85C5);
+
+    function setUp() public {
+        vm.createSelectFork("mainnet", 17_806_055);
+        vm.deal(address(this), 1_000_001 ether);
+    }
 
     function testHack() public {
-        console.log("testHack");
+        console.log("balance before", address(this).balance);
+
+        uint256[2] memory amount;
+        amount[0] = 40_000 ether;
+        amount[1] = 0;
+        pool.add_liquidity{value: 40_000 ether}(amount, 0);
+
+        console.log("balance after addliq", address(this).balance);
     }
 
 }
